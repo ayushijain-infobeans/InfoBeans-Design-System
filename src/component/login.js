271f05656a9ccd28fe2c102a456488346b1a7c8b
ayushijain0708 <82175024+ayushijain0708@users.noreply.github.com>
2021-05-12 04:08:20 +0530

    Authentication Done

--- src/component/login.js
+++ src/component/login.js
@@ -1,50 +1,89 @@
-import React from "react";
-import "../style/login.css";
+import React from 'react'
+import { useHistory } from "react-router-dom";
+  import "../style/login.css";
+
+
+const user={
+    email:"ayushi@gmail.com",
+    password:"ayushi123"
+}
+
 
 function Login() {
+  let history=useHistory();
+  if(sessionStorage.token){
+    history.push("/")
+  }
+
+const loginCheck=(e)=>{
+  e.preventDefault()
+  let email=e.target.elements[0].value;
+  let password=e.target.elements[1].value;
+  if(email===user.email && password===user.password){
+    console.log("true")
+    sessionStorage.setItem("token",email)
+     history.push("/photoGallery")
+  }
+  else{
+    alert("Invalid Credentials");
+  }
+}
     return (
-        <div className="login">
-            <form className="form-login">
-                <nav class="navbar navbar-light">
-                    <a class="navbar-brand text-white" href="#">
-                        <img src="https://infobeans-design-system.web.app/images/logo-infobeans-white.svg"  class ="nav-img" />
-
-                         <span class="Intranet">Intranet Portal</span> 
-                    </a>
-                </nav>
-
-                <div class="login-container">
-                    <div class="logo">
-                        <img src="https://infobeans-design-system.web.app/images/logo-infobeans-black.svg" />
-                    </div>
-                    <div class="form-group">
-                        <label class="form-label">Email</label>
-                        <div class="form-field">
-                            <input type="text" class="field" placeholder="Your InfoBeans email address" />
-                        </div>
-
-                    </div>
-<br/>
-                    <div class="form-group">
-                      
-                        <label class="form-label-password">Password   <a class="float-right forgot" href="#">Forgot?</a></label>
-                        <div class="form-field">
-                            <input type="text" class="field" placeholder="Your Password" />
-                        </div>
-                    </div>
-
-                    <br/>
-
-                        <button class="button">Login to Intranet Portal</button>
+    
+    <div className="login">
+      <div className="header">
+        <nav className="navbar  navbar-light">
+          <div className="container-fluid">
+            <a className="navbar-brand text-white" href="#">
+              <img
+               src= "https://infobeans-design-system.web.app/images/logo-infobeans-white.svg" className="nav-img"/>
+                  <span className="Intranet">Intranet Portal</span>
+            </a>
+            <ul className="nav justify-content-end">
+              <li className="nav-item text-secondary">
+                <i className="fas fa-th fa-2x"></i>
+              </li>
+              </ul>
+          </div>
+        </nav>
+      </div>
+      <div className="login-container">
+        <div className="logo">
+          <div className="login-Img">
+          <img src="https://infobeans-design-system.web.app/images/logo-infobeans-black.svg" />
+          </div>
+          <form onSubmit={loginCheck}>
+          <div className="form-group">
+            <label className="form-label">Email</label>
+            <div className="form-field">
+              <input type="text" className="field" placeholder="Your InfoBeans email address" />
+            </div>
+          </div>
+          <br/>
+          <div className="form-group">
+            <label className="form-field"
+            >Password 
+              <a href="#"className="float-right forgot">Forgot?</a></label>
+            <div className="form-field">
+              <input type="password"  className="form-control" placeholder="Your password" />
+            </div>
+          </div>
+          <br/>
+          <div className="form-field">
+                    <button className="button" >Login to Intranet Portal</button>
                 </div>
-            </form>
-            <footer class="footer mt-3">
-                <p> &copy; Copyright 2020 InfoBeans. All Rights Reserved.</p>
-            </footer>
-
+                </form>
         </div>
-
+      </div>
+      <div className="footer">
+         
+          &copy; Copyright 2020 InfoBeans. All Rights Reserved.
+      
+      </div>
+    </div>
+    
     );
 }
 
-export default Login;
+export default Login
+
