ff45b6086cbeb5a2c1610e826bcfb15e9cef7aba
ayushijain0708 <82175024+ayushijain0708@users.noreply.github.com>
2021-05-12 04:49:40 +0530

    Restructured in React

--- src/component/photoGallery.js
+++ src/component/photoGallery.js
@@ -3,46 +3,46 @@ import '../style/photoGallery.css'
 
 function photoGallery(){
     return (
-        <div class="home-container">
-             <h3 class="home-stretch color-grey ">Recent Photo Galleries
-            <span>
-                <a href="#" class="see-all">See More <span> &#62;</span></a>
-            </span>
+       <div class="home container mt-5">
+
+        <h3 class="home-stretch">Recent Photo Galleries
+            
+                 <a href="#" class="see-all">See More <span> &#62;</span></a>
 
         </h3>
-        <div class="row">
-            <div class="col-md-3">
-                <div class="card">
-                    <img src="../image/sample-profile-image.jpg" class="card-img-top" alt="..." height="226px"/>
-                    <div class="card-body">
+         <div class="row mt-4">
+             <div class="col-md-3">
+                 <div class="card">
+                   <img src="https://infobeans-intranet.web.app/assets/images/img-card-gallery.png" class="card-img-top" alt="..."/>
+                     <div class="card-body">
 
-                        <p class="card-text"> International Women’s day 2020 Celebration – CITP Indore office</p>
-                        <a href="#" class="facebook">View on Facebook <span> &#62;</span> </a>
-                    </div>
+                         <p class="card-text"> International Women’s day 2020 Celebration – CITP Indore office</p>
+                         <a href="#" class="facebook">View on Facebook <span> &#62;</span> </a>
+                     </div>
                 </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card">
-                    <img src="../images/card-image.jfif" class="card-img-top" alt="..." height="226px"/>
-                    <div class="card-body">
+             </div>
+             <div class="col-md-3 px-0">
+                 <div class="card">
+                     <img src="https://infobeans-intranet.web.app/assets/images/img-card-gallery.png" class="card-img-top" alt="..." />
+                     <div class="card-body">
 
-                        <p class="card-text"> International Women’s day 2020 Celebration – CITP Indore office</p>
-                        <a href="#" class="facebook">View on Facebook <span> &#62;</span></a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3">
+                         <p class="card-text"> International Women’s day 2020 Celebration – CITP Indore office</p>
+                         <a href="#" class="facebook">View on Facebook <span> &#62;</span></a>
+                     </div>
+             </div>
+             </div>
+            <div class="col-md-3 px-0">
                 <div class="card">
-                    <img src="../images/card-image.jfif" class="card-img-top" alt="..." height="226px"/>
-                    <div class="card-body">
+                     <img src="https://infobeans-intranet.web.app/assets/images/img-card-gallery.png" class="card-img-top" alt="..." />
+                     <div class="card-body">
 
-                        <p class="card-text"> International Women’s day 2020 Celebration – CITP Indore office</p>
-                        <a href="#" class="facebook">View on Facebook <span> &#62;</span></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                         <p class="card-text"> International Women’s day 2020 Celebration – CITP Indore office</p>
+                         <a href="#" class="facebook">View on Facebook <span> &#62;</span></a>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
     
     );
 }
