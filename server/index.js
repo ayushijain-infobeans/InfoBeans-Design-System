f9714040c521dd5255050b885a1c7c3caef9b170
ayushijain0708 <82175024+ayushijain0708@users.noreply.github.com>
2021-05-18 04:23:40 +0530

    multer added

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fileRoutes = require('./routes/fileUploadroutes');
 
+const port = process.env.PORT || 9000;
+const app = express();
+app.use(cors());
+
+require('./database')();
+
+app.use(bodyParser.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+console.log( path.join(__dirname, 'uploads'))
+app.use('/api', fileRoutes.routes);
+
+app.listen(port, () => console.log(`server is listening on url http://localhost:${port}`));
