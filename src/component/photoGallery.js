17296172c25bd50cd029988c83119b9f00e2abf2
ayushijain0708 <82175024+ayushijain0708@users.noreply.github.com>
2021-05-18 06:15:42 +0530

    PhotoGallery Styling

--- src/component/photoGallery.js
+++ src/component/photoGallery.js
@@ -1,51 +1,56 @@
-import react from 'react'
+import React, { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom';
 import '../style/photoGallery.css'
 import Navbar from './Navbar'
+import Card from './card'
+import axios from 'axios'
 
-function photoGallery(){
+function PhotoGallery(props) {
+    let history = useHistory()
+    if (!sessionStorage.token) {
+        history.push("/login")
+    }
+
+    const [member, setmember] = useState([])
+
+    useEffect(() => {
+        console.log("hello")
+        axios({
+            method: 'get',
+            url: "http://localhost:9000/api/getSingleFiles"
+        }).then((response) => {
+
+            console.log("hello ayushi" + response.data)
+            setmember(response.data)
+
+        }, (error) => {
+            console.log("Error from api", error)
+        })
+    }, []);
+
+    console.log(props.data)
     return (
-        <> <Navbar/>
-       <div class="home container mt-5">
+        <><Navbar />
+<br/><br/>
+            <div clasName="home-container" style={{paddingLeft:"192px", margin:"29px"}}>
+                    <h2 className="home-stretch">Photo Gallery
+                    
+                    <a href="#" class="see-all">See More <span> &#62;</span></a></h2>
+                    <br/>
+<div className="row">
+    {member.map(function (each) {
+                    console.log("each", each);
+                    return <Card data={each} key={each._id} />;
+                })}
+</div>
+                
+            </div>
 
-        <h3 class="home-stretch">Recent Photo Galleries
-            
-                 <a href="#" class="see-all">See More <span> &#62;</span></a>
 
-        </h3>
-         <div class="row mt-4">
-             <div class="col-md-3">
-                 <div class="card">
-                   <img src="https://infobeans-intranet.web.app/assets/images/img-card-gallery.png" class="card-img-top" alt="..."/>
-                     <div class="card-body">
-
-                         <p class="card-text"> International Women’s day 2020 Celebration – CITP Indore office</p>
-                         <a href="#" class="facebook">View on Facebook <span> &#62;</span> </a>
-                     </div>
-                </div>
-             </div>
-             <div class="col-md-3 ">
-                 <div class="card">
-                     <img src="https://infobeans-intranet.web.app/assets/images/img-card-gallery.png" class="card-img-top" alt="..." />
-                     <div class="card-body">
-
-                         <p class="card-text"> International Women’s day 2020 Celebration – CITP Indore office</p>
-                         <a href="#" class="facebook">View on Facebook <span> &#62;</span></a>
-                     </div>
-             </div>
-             </div>
-            <div class="col-md-3">
-                <div class="card">
-                     <img src="https://infobeans-intranet.web.app/assets/images/img-card-gallery.png" class="card-img-top" alt="..." />
-                     <div class="card-body">
-
-                         <p class="card-text"> International Women’s day 2020 Celebration – CITP Indore office</p>
-                         <a href="#" class="facebook">View on Facebook <span> &#62;</span></a>
-                     </div>
-                 </div>
-             </div>
-         </div>
-     </div>
-    </>
-    );
+        </>
+    )
 }
-export default photoGallery;
+export default PhotoGallery
+
+
+
