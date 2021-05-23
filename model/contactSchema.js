3bbc13eb2024f71d13b1fdbe6578a124ce1d5eb3
ayushijain0708 <82175024+ayushijain0708@users.noreply.github.com>
2021-05-24 01:13:42 +0530

    small modification

--- model/contactSchema.js
+++ model/contactSchema.js
@@ -12,31 +12,3 @@ const contactSchema= new Schema({
 
 const Contact=mongoose.model("Contact",contactSchema)
 module.exports=Contact;
-// const mongoose = require('mongoose')
-// const Schema = mongoose.Schema;
-// const contactSchema = new Schema({
-//     firstname:{
-//         typr:String,
-//         required:true,
-//     },
-//     lastname:{
-//         type:String,
-//         required:true,
-//     },
-//     email:{
-//         type:String,
-//         unique:true,
-//         required:true,
-//     },
-//     description:{
-//         type:String,
-//         required:true,
-//     },
-//     file:{
-//         type:String,
-//         required:true
-//     },
-// })
-
-// const Contact= mongoose.model("Contact", contactSchema)
-// module.exports=Contact;
